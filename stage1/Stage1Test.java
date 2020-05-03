482a2ef767f963695021a2300387ae5e9b7b91b7
felipeville <38149365+felipeville@users.noreply.github.com>
2020-05-02 21:13:15 -0400

    Update Stage1Test.java

--- stage1/Stage1Test.java
+++ stage1/Stage1Test.java
@@ -7,7 +7,7 @@ public class Stage1Test  {
         
         Locale.setDefault(Locale.US);  // to read number in US format, like 1.5 (not like 1,5)
         Scanner in = new Scanner(new File(arg[0]));
-        float time = 0.0f;
+        float time = 0.0f;     // when time = 2.5, it's actually 2.4999999... 
         Joystick l_Joy = new Joystick();
         Joystick r_Joy = new Joystick();
         Operator operator = new Operator(in, l_Joy, r_Joy);
