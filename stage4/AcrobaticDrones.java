f1f378f05b93bc47c7cc15ec11d61be59d9a39ff
felipeville <38149365+felipeville@users.noreply.github.com>
2020-05-17 18:40:45 -0400

    Update and rename Stage4Test.java to AcrobaticDrones.java

--- stage4/AcrobaticDrones.java
+++ stage4/AcrobaticDrones.java
@@ -4,7 +4,7 @@ import java.util.Locale;
 import java.util.ArrayList;
 import java.lang.Math;
 
-public class Stage4Test  {
+public class AcrobaticDrones  {
     static {
        Locale.setDefault(Locale.US);
        t0=System.currentTimeMillis();  // time in ms since app 1970.
